411aa2b2bedca76296815c4917b63f799623cd5d
bhaskar <bhaskarkumar.india@gmail.com>
2024-09-23 22:52:44 +0530

    string intro

---
+++ src/string/Main.java
@@ -0,0 +1,10 @@
+package string;
+
+public class Main {
+	public static void main(String[] str) {
+		StringPractice sp=new StringPractice();
+		
+		sp.displayChar("Bhaskar Kumar");
+	}
+
+}
---
+++ src/string/StringPractice.java
@@ -0,0 +1,9 @@
+package string;
+
+public class StringPractice {
+	public void displayChar(String s) {
+		for(int i=0;i<s.length();i++) {
+			System.out.print(s.charAt(i)+" ");
+		}
+	}
+}
