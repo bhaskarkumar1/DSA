504cca4aec5b9781e30adf10b000d899e38407ed
bhaskar <bhaskarkumar.india@gmail.com>
2024-10-05 01:18:40 +0530

    codingbat recursion1++

--- src/recursionquestion/Main.java
+++ src/recursionquestion/Main.java
@@ -10,6 +10,12 @@ public class Main {
 		System.out.println(qs.fact(5));
 		System.out.println(qs.fib(32));
 		System.out.println(qs.sumOfDigit(1234));
+		System.out.println(qs.count7(71767676));
+		System.out.println(qs.power(2,5));
+		System.out.println(qs.countX("xxhxixx"));
+		System.out.println(qs.countHi("xhixhixhi"));
+		System.out.println(qs.changeXY("xhixhix"));
+		System.out.print(qs.changePi("pibhaskarpi21"));
 	}
 
 }
--- src/recursionquestion/Question.java
+++ src/recursionquestion/Question.java
@@ -37,5 +37,45 @@ public class Question {
 		
 		return n%10+sumOfDigit(n/10);
 	}
+//	count the digit 7 in  number 
+	public int count7(int n) {
+		if(n==0) return 0;
+		
+		if(n%10==7) return 1+count7(n/10);
+		return count7(n/10);
+	}
+// power 
+	public int power(int base,int n) {
+		if(n==0) return 1;
+		return base*power(base,n-1);
+	}
+//	countX("xxhixx") → 4
+	public int countX(String s) {
+		if(s.length()==0) return 0;
+		if(s.startsWith("x")) return 1+countX(s.substring(1));
+	return countX(s.substring(1));
+	}
+//  countHi("xhixhix") → 2
+	public int countHi(String s) {
+		if(s.length()<2) return 0;
+		if(s.startsWith("hi")) return 1+countHi(s.substring(2));
+		
+		return countHi(s.substring(1));
+		
+	}
+
+//	changeXY("xhixhix") → "yhiyhiy"
+	public String changeXY(String s) {
+		if(s.length()==0) return "";
+		if(s.startsWith("x")) return "y" +changeXY(s.substring(1));
+		return s.substring(0,1)+ changeXY(s.substring(1));
+	}
+	
+//	changePi("pipi") → "3.143.14"
+	public String changePi(String str) {
+		if(str.length()==0) return "";
+		if(str.length()>=2 && str.startsWith("pi")) return "3.14"+changePi(str.substring(2));
+		return str.substring(0,1)+str.substring(1);
+	}
 	
 }
