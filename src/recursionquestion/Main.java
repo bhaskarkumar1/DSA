6b6e77d956ae81b82891bfed128d006e1843effa
bhaskar <bhaskarkumar.india@gmail.com>
2024-10-05 18:25:53 +0530

    some more string recursion question

--- src/recursionquestion/Main.java
+++ src/recursionquestion/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Question qs=new Question();
 //		qs.allSubstring("abc");
+		int[] arr= {1,2,5,4,6,7,11,11,5,6,7,8,9,11,2,20};
 		
 		System.out.println(qs.fact(5));
 		System.out.println(qs.fib(32));
@@ -15,7 +16,15 @@ public class Main {
 		System.out.println(qs.countX("xxhxixx"));
 		System.out.println(qs.countHi("xhixhixhi"));
 		System.out.println(qs.changeXY("xhixhix"));
-		System.out.print(qs.changePi("pibhaskarpi21"));
+		System.out.println(qs.changePi("pibhaskarpi21"));
+		System.out.println(qs.noX("xaxbxasdx"));
+		System.out.println(qs.array6(arr, 0));
+		System.out.println(qs.array11(arr, 0));
+		System.out.println(qs.array220(arr, 0));
+		System.out.println(qs.allStar("hello"));
+		System.out.println(qs.endX("hihixxx"));
+		System.out.println(qs.countPairs("axaxqqq"));
+		System.out.println(qs.pairs("aaaaa"));
 	}
 
 }
--- src/recursionquestion/Question.java
+++ src/recursionquestion/Question.java
@@ -5,6 +5,8 @@ public class Question {
 	
 //	the brute force - 1st loop
 	
+	private static final String String = null;
+
 	public void allSubstring(String s) {
 		for(int i=0;i<s.length();i++) {
 			for(int j=i+1;j<=s.length();j++) {
@@ -77,5 +79,82 @@ public class Question {
 		if(str.length()>=2 && str.startsWith("pi")) return "3.14"+changePi(str.substring(2));
 		return str.substring(0,1)+str.substring(1);
 	}
+//	noX("xaxb") → "ab"
 	
+	public String noX(String str) {
+		if(str.length()==0) return "";
+		
+		if(str.startsWith("x")) return ""+noX(str.substring(1));
+		else return str.substring(0,1)+noX(str.substring(1));
+	}
+//	array6([1, 6, 4], 0) → true
+public boolean array6(int[] nums,int index) {
+	if(index==nums.length) return false;
+	if(nums[index]==6) return true;
+	return array6(nums,index+1);
+}
+
+//array11([1, 2, 11], 0) → 1
+public int array11(int[] nums,int index) {
+	if(index==nums.length) return 0;
+	if(nums[index]== 11) return 1+array11(nums,index+1);
+	return array11(nums,index+1);
+}
+
+//Given an array of ints, compute recursively if the array contains somewhere a value followed in the array by that value times 10. We'll use the convention of considering only the part of the array that begins at the given index.
+public boolean array220(int[] nums,int index) {
+	if(index==nums.length) return false;
+	if(index+1<nums.length && nums[index]*10==nums[index+1]) return true;
+	
+	return array220(nums,index+1);
+}
+//allStar("hello") → "h*e*l*l*o"
+
+
+public String allStar(String str) {
+	if(str.length()==0) return "";
+	if(str.length()==1) return str;
+	
+	return str.substring(0,1)+"*"+allStar(str.substring(1));
+	
+}
+//endX("xhixhix") → "hihixxx"
+public String endX(String str) {
+return withNoX(str) + withX(str);
+}
+
+public String withNoX(String str) {
+if(str.length()==0) return "";
+if(str.substring(0,1).equals("x")) {
+	return withNoX(str.substring(1));
+}
+return str.substring(0,1)+withNoX(str.substring(1));
+}
+
+public String withX(String str) {
+	if(str.length()==0) return "";
+	if(str.substring(0,1).equals("x")) {
+		return "x"+withX(str.substring(1));
+	}
+	return withX(str.substring(1));
+}
+
+//We'll say that a "pair" in a string is two instances of a char separated by a char. So "AxA" the A's make a pair. Pair's can overlap, so "AxAxA" contains 3 pairs -- 2 for A and 1 for x.
+public int countPairs(String str) {
+	if(str.length()<3) return 0;
+	
+	if(str.charAt(0) == str.charAt(2)) return 1+countPairs(str.substring(1));
+	
+	return countPairs(str.substring(1));
+}
+//pairStar("xxyy") → "x*xy*y"
+
+public String pairs(String str) {
+	if(str.length()==1) return str;
+	if(str.charAt(0)==str.charAt(1)) return str.charAt(0)+"*"+pairs(str.substring(1));
+	return str.charAt(0)+pairs(str.substring(1));
+}
+
+
+
 }
